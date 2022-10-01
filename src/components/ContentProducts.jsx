4505278d33f676083d8010fe3fd1e9d893d416c5
GHuizzi <huizzi2910@gmail.com>
2022-09-30 22:39:04 -0400

    add components types products and styles

---
+++ src/components/ContentProducts.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/stylesVariables/stylesContent/contentTypes.css";
+function ContentProducts() {
+  return (
+    <div>
+      <div className="types">
+        <p>featured products </p>
+      </div>
+    </div>
+  );
+}
+export default ContentProducts;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import imageHero2 from "../images/imageHero2.jpg";
 import Wavy from "../components/icons/Wavy";
 import "../styles/home.css";
+import ContentProducts from "../components/ContentProducts";
 function Home() {
   return (
     <div>
@@ -9,6 +10,9 @@ function Home() {
         <img className="hero1" src={imageHero2} alt="imageHero1" />
         <div className="bgWavy">
           <Wavy width="100%" />
+        </div>
+        <div className="typesProducts">
+          <ContentProducts />
         </div>
       </div>
     </div>
