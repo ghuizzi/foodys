3a68a8dd3ec77ef3b10ffa037d6616dc99865ff6
GHuizzi <huizzi2910@gmail.com>
2022-10-02 17:38:49 -0400

    fix styles content types products

--- src/components/ContentProducts.jsx
+++ src/components/ContentProducts.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "../styles/stylesVariables/stylesContent/contentTypes.css";
+import spaghetti from "../images/icons/spaghetti.png";
+import bean from "../images/icons/bean.png";
+import bread from "../images/icons/bread.png";
+import salad from "../images/icons/salad.png";
 function ContentProducts() {
   return (
     <div>
       <div className="types">
-        <p>featured products </p>
+        <div className="categories">
+          <img src={spaghetti} alt="" />
+        </div>
+        <div className="categories">
+          <img src={bean} alt="" />
+        </div>
+        <div className="categories">
+          <img src={bread} alt="" />
+        </div>
+        <div className="categories">
+          <img src={salad} alt="" />
+        </div>
       </div>
     </div>
   );
